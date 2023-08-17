8443af917051c4668189acd5237db7d26733b621
Leslie Saliba <lesliesaliba@gmail.com>
2023-08-17 17:07:15 +0300

    Challenge 1 - Done with Step 7

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -74,3 +74,62 @@ let string2 = "goodbye";
 let result = string1.concat(string2);
 console.log(result);
 
+//Step 7
+let number1 = 1;
+let string1bis = "1";
+let booleanTrue = true;
+let object1 = { value: 1 };
+
+// Strict Equality (===) - Checks for both value and data type
+console.log(number1 === number1);    // true
+console.log(number1 === string1bis);    // false (Number ≠ String)
+console.log(number1 === booleanTrue);   // false (Number ≠ Boolean)
+console.log(number1 === object1);    // false (Number ≠ Object)
+
+// Strict Inequality (!==) - Checks for value and data type
+console.log(number1 !== number1);    // false 
+console.log(number1 !== string1bis);    // true 
+console.log(number1 !== booleanTrue);   // true 
+console.log(number1 !== object1);    // true 
+
+// Loose Equality (==) - Converts types and checks for value equality
+console.log(number1 == number1);     // true 
+console.log(number1 == string1bis);     // true (same value, after conversion into strings)
+console.log(number1 == booleanTrue);    // true (same value as true is converted to 1 and false is converted to 0)
+console.log(number1 == object1);     // false (So numbers can't be equal to objects?) 
+
+// Loose Inequality (!=) - Converts types and checks for value inequality
+console.log(number1 != number1);     // false 
+console.log(number1 != string1bis);     // false
+console.log(number1 != booleanTrue);    // false
+console.log(number1 != object1);     // true (So numbers can't be equal to objects?) 
+
+
+let number5 = 5;
+let string10 = "10";
+let booleanT = true;
+let booleanF = false;
+
+// Greater than (>)
+console.log(number5 > 3);         // true 
+console.log(number5 > 7);         // false 
+console.log(string10 > "5");       // false (strings and numbers are compared character by character)
+console.log(booleanT > 0);    // true (true is converted to 1)
+
+// Greater than or equals (>=)
+console.log(number5 >= 5);        // true
+console.log(number5 >= 3);        // true
+console.log(string10 >= "5");      // false (strings and numbers are compared character by character)
+console.log(booleanF >= 0);  // true (false is converted to 0)
+
+// Less than (<)
+console.log(number5 < 8);         // true 
+console.log(number5 < 3);         // false 
+console.log(string10 < "8");       // true (strings and numbers are compared character by character)
+console.log(booleanT < 2);    // true (true is converted to 1)
+
+// Less than or equals (<=)
+console.log(number5 <= 5);        // true 
+console.log(number5 <= 8);        // true 
+console.log(string10 <= "10");     // true (strings and numbers are compared character by character)
+console.log(booleanF <= 0);  // true (false is converted to 0)
