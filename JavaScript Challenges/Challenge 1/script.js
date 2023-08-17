44cfbb767ffc234f698fab7dcf6b10466404ebb6
Leslie Saliba <lesliesaliba@gmail.com>
2023-08-17 15:26:08 +0300

    Challenge 1 - Done with Step 4

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -20,3 +20,26 @@ age = 25
 
 console.log(age, lastName, DOB)
 
+console.log("This is a text")
+console.log(true)
+console.log(false)
+console.log(1215)
+console.log("999")
+console.log({}) // object (can be object, array, date)
+console.log([]) // array
+console.log(null)
+console.log(undefined)
+
+let text = "This is a random text."
+let numbers = 123
+let suite = ["array1", 2]
+let car = {type:"Mercedes", color:"white"}
+let x = true 
+let y 
+
+console.log(text + " string")
+console.log(numbers + " number")
+console.log(suite + " array")
+console.log(car + " object")
+console.log(x + " boolean")
+console.log(y + " undefined")
