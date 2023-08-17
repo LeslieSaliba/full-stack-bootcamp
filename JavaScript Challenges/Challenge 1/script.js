dce3d73ae6149e7e5d26fc98ebab18252bac0f27
Leslie Saliba <lesliesaliba@gmail.com>
2023-08-17 10:56:48 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -6,3 +6,17 @@ alert(name)
 function alertBox() {
     alert(name);
 }
+
+var age
+const lastName = "Saliba"
+let DOB
+
+age = 24
+DOB = "25 October 1998"
+
+console.log(age)
+
+age = 25
+
+console.log(age, lastName, DOB)
+
