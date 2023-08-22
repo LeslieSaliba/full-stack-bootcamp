0bb6a69a7f6c91b3e18e6b6c52ef3d8491165401
Leslie Saliba <lesliesaliba@gmail.com>
2023-08-22 15:19:59 +0300

    Challenge 4 - Done with step 3

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -1,19 +1,34 @@
 // Step 1: Arrays and Objects
 
-let fiveNumbers = [1, 7, 8, 25, 29]; 
-console.log(fiveNumbers); 
+let firstNum; // declare variables before destructuration 
+let secondNum;
+let thirdNum;
 
-let personalInfo = {name:"Leslie", age:24, height:"163cm", location:"Lebanon", nationality:"French-Lebanese"};
-console.log(personalInfo); 
+let fiveNumbers = [firstNum, secondNum, thirdNum, fourthNum, fifthNum,] = [1, 7, 8, 25, 29];
+console.log(fiveNumbers);
+
+let name; // same as above 
+let age;
+let height;
+let locationn;
+let nationality;
+
+let personalInfo = { name, age, height, locationn, nationality } = { name: "Leslie", age: 24, height: "163cm", locationn: "Lebanon", nationality: "French-Lebanese" };
+console.log(personalInfo);
 
 // Step 2: Spread Operator
 
-let nextThreeNumbers = [21, 57, 64]; 
+let nextThreeNumbers = [21, 57, 64];
 
-let allNumbers = [...fiveNumbers, ...nextThreeNumbers]; 
-console.log(allNumbers); 
+let allNumbers = [...fiveNumbers, ...nextThreeNumbers];
+console.log(allNumbers);
 
-let additionalInfo = {hobby:"design", favoriteDish:"lasagna"}; 
-let fullInfo = {...personalInfo, ...additionalInfo}; 
-console.log(fullInfo); 
+let additionalInfo = { hobby: "design", favoriteDish: "lasagna" };
+let fullInfo = { ...personalInfo, ...additionalInfo };
+console.log(fullInfo);
+
+// Step 3: Destructuring
+console.log(firstNum, secondNum, thirdNum);
+
+console.log(name, locationn, nationality); // Error in console: location (single n) already defined in js 1:1 
 
