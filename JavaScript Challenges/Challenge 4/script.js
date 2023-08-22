09908c68b4a3293d1cdbe951b37ce20038cddeee
Leslie Saliba <lesliesaliba@gmail.com>
2023-08-22 16:12:46 +0300

    Challenge 4 - Done with step 4

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -4,7 +4,8 @@ let firstNum; // declare variables before destructuration
 let secondNum;
 let thirdNum;
 
-let fiveNumbers = [firstNum, secondNum, thirdNum, fourthNum, fifthNum,] = [1, 7, 8, 25, 29];
+// destructuration below 
+let fiveNumbers = [firstNum, secondNum, thirdNum, ...restOfNumbers] = [1, 7, 8, 25, 29];
 console.log(fiveNumbers);
 
 let name; // same as above 
@@ -13,8 +14,11 @@ let height;
 let locationn;
 let nationality;
 
-let personalInfo = { name, age, height, locationn, nationality } = { name: "Leslie", age: 24, height: "163cm", locationn: "Lebanon", nationality: "French-Lebanese" };
+let contactInfo = { email: 'leslie@gmail.com', number: 812 } // object created to try nested destructuring
+
+let personalInfo = { name, age, height, locationn, nationality, contactInfo: { email, number } } = { name: "Leslie", age: 24, height: "163cm", locationn: "Lebanon", nationality: "French-Lebanese", contactInfo };
 console.log(personalInfo);
+
 
 // Step 2: Spread Operator
 
@@ -27,8 +31,22 @@ let additionalInfo = { hobby: "design", favoriteDish: "lasagna" };
 let fullInfo = { ...personalInfo, ...additionalInfo };
 console.log(fullInfo);
 
+
 // Step 3: Destructuring
 console.log(firstNum, secondNum, thirdNum);
 
 console.log(name, locationn, nationality); // Error in console: location (single n) already defined in js 1:1 
 
+
+// Step 4: Advanced Operations
+console.log("email alone: " + email, "number alone: " + number, "both contact info: " + contactInfo);
+
+console.log(restOfNumbers); // how to transform it into a new array?
+
+let object1 = { ingredient: "tomato", fruit: "yes", color: "red", price: 1 };
+let object2 = { ingredient: "onion", vegetable: "yes", color: "white", price: 1 };
+console.log(object1);
+console.log(object2);
+
+let objectMerge = { ...object1, ...object2 }; // if commun properties in merged objects, take the values of the second/last objet
+console.log(objectMerge); 
