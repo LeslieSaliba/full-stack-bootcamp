b42d34a9c20157a278c8c83aca54c99889ce2a6b
Leslie Saliba <lesliesaliba@gmail.com>
2023-08-22 14:55:35 +0300

    Challenge 4 - Done with step 2

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -3,7 +3,17 @@
 let fiveNumbers = [1, 7, 8, 25, 29]; 
 console.log(fiveNumbers); 
 
-let personalInfo = car = {name:"Leslie", age:24, height:"163cm", location:"Lebanon", nationality:"French-Lebanese"};
+let personalInfo = {name:"Leslie", age:24, height:"163cm", location:"Lebanon", nationality:"French-Lebanese"};
 console.log(personalInfo); 
 
+// Step 2: Spread Operator
+
+let nextThreeNumbers = [21, 57, 64]; 
+
+let allNumbers = [...fiveNumbers, ...nextThreeNumbers]; 
+console.log(allNumbers); 
+
+let additionalInfo = {hobby:"design", favoriteDish:"lasagna"}; 
+let fullInfo = {...personalInfo, ...additionalInfo}; 
+console.log(fullInfo); 
 
