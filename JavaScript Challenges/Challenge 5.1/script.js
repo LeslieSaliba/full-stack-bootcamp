5ab2d1bd31af27a3b6cb2dfc7372f3a8a11b517b
Leslie Saliba <lesliesaliba@gmail.com>
2023-08-24 16:49:50 +0300

    Challenge 5.1 - Not done with step 3

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -54,3 +54,45 @@ function toggleVisibility() {
     }
 }
 
+// Step 3 
+
+let form = document.createElement('form');
+let fullName = document.createElement('input');
+fullName.type = "text";
+
+let emailAddress = document.createElement('input');
+emailAddress.type = "email";
+
+let password = document.createElement('input');
+password.type = "password";
+
+let confirmPassword = document.createElement('input');
+confirmPassword.type = "password";
+
+let submitButton = document.createElement('input');
+submitButton.type = "submit";
+
+document.body.appendChild(form);
+form.append(fullName, emailAddress, password, confirmPassword, submitButton);
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (password.value !== confirmPassword.value) {
+        let errorPassword = document.createElement('p');
+        errorPassword.textContent = "Passwords don't match."
+        document.body.appendChild(errorPassword);
+    }
+});
+
+submitButton.addEventListener('click', function () {
+    let fullName = form.fullName.value; // error in console: Cannot read properties of undefined (reading 'value')
+    let emailAddress = form.emailAddress.value;
+    let password = form.password.value;
+    let confirmPassword = form.confirmPassword.value;
+
+    if (fullName && emailAddress && password && password === confirmPassword) {
+        let successMessage = document.createElement('p');
+        successMessage.textContent = "Your form has been submitted."
+        document.body.appendChild(successMessage);
+    }
+}); 
