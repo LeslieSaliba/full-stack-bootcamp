84423d470b239130ba774f6b479464f702951340
Leslie Saliba <lesliesaliba@gmail.com>
2023-08-24 16:06:09 +0300

    Challenge 5.1 - Done with step 2

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -17,3 +17,40 @@ button.addEventListener("click", function () {
     let randomNumber = Math.floor(Math.random() * 1000) + 1;
     button.textContent = `Clicked ${randomNumber}!`;
 });
+
+// Step 2 
+
+let h1 = document.createElement("h1");
+h1.textContent = "This is a h1";
+document.body.appendChild(h1);
+
+h1.style.border = "10px dotted green";
+
+document.addEventListener("keydown", function () {
+    let key = event.key.toLowerCase();
+
+    if (key === "a") {
+        h1.style.backgroundColor = "red";
+    }
+    else if (key === "s") {
+        h1.style.marginLeft = "10px";
+    } else if (key === "d") {
+        let paragraph = document.createElement("p");
+        paragraph.textContent = "Key D was pressed!";
+        document.body.appendChild(paragraph);
+    } else if (event.key === "w") {
+        toggleVisibility();
+    }
+    else if (key === " ") {
+        h1.style.fontSize = "50px";
+    }
+});
+
+function toggleVisibility() {
+    if (h1.style.display === "none") {
+        h1.style.display = "block";
+    } else {
+        h1.style.display = "none";
+    }
+}
+
