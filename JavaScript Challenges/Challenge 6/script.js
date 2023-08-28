8f63296c21fadad87a3a02d8a8e0589266bd5a43
Leslie Saliba <lesliesaliba@gmail.com>
2023-08-28 15:00:08 +0300

    Challenge 7 - Done with step 1

--- JavaScript Challenges/Challenge 6/script.js
+++ JavaScript Challenges/Challenge 6/script.js
@@ -1,5 +1,7 @@
 // Step 1: Introduction to Asynchronous JS
 
+// setTimeout(function, milliseconds) = toujours 2 paramètres 
+
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     let helloMessage = document.createElement("p");
@@ -32,6 +34,11 @@ function updateClock() {
 
 setInterval(updateClock, 1000); // update every second
 
+// setInterval(() => {
+//   let currentTime = new Date().toLocaleTimeString;
+//   console.log(currentTime);
+// }, 1000);
+
 
 // Promises in JavaScript are a programming pattern that provides a structured way to work with asynchronous operations
 // A promise represents a value that may be available now, or in the future, or never. Promises have three possible states:
@@ -39,7 +46,7 @@ setInterval(updateClock, 1000); // update every second
 // 2. Fulfilled: The state when the asynchronous operation is completed successfully, and the promise resolves with a value.
 // 3. Rejected: The state when the asynchronous operation encounters an error or fails, and the promise is rejected with a reason.
 
-const delayedPromise = new Promise((resolve, reject) => { // created a new promise using the Promise constructor, which takes a single argument (i.e. a function with two parameters: resolve and reject)
+const delayedPromise = new Promise((resolve) => { // created a new promise using the Promise constructor, which takes a single argument (i.e. a function with two parameters: resolve and reject)
   setTimeout(() => { // used the setTimeout function to introduce a delay of 2 seconds
     resolve("Promise resolved!"); // after the delay, the resolve function is called with the message "Promise resolved!"
   }, 2000); // 2000 milliseconds = 2 seconds
@@ -55,6 +62,8 @@ delayedPromise.then(() => {
   .then((chainedMessage) => {
     console.log(chainedMessage); //  Logs "Chained message!"
   });
+
+// voir correction, pas très clair 
 
 
 // Step 2: Diving Deeper into Async Operations
@@ -106,10 +115,10 @@ const fetchPostsData = async () => {
   fetchPostsData();
 
   // # Ensuring All Requests Complete Before Processing Results:
-      // When you have a list of promises that you want to execute in parallel, 
-      // you can use Promise.all to ensure that all promises are completed before you start processing the results. 
-      // Promise.all takes an array of promises and returns a new promise that resolves to an array of results 
-      // when all input promises have resolved.
+  // When you have a list of promises that you want to execute in parallel, 
+  // you can use Promise.all to ensure that all promises are completed before you start processing the results. 
+  // Promise.all takes an array of promises and returns a new promise that resolves to an array of results 
+  // when all input promises have resolved.
 
   const fetchAndProcessPostsSequentially = async () => {
     const postIds = [4, 5, 6, 7, 8];
---
+++ JavaScript Challenges/Challenge 7/script.js
@@ -0,0 +1,81 @@
+// Step 1
+
+let container = document.createElement('div');
+container.className = "container";
+
+let loginContainer = document.createElement('div');
+loginContainer.className = "login__container";
+
+let loginSignIn = document.createElement('div');
+loginSignIn.className = "login__signin";
+
+let loginSignInNow = document.createElement('div');
+loginSignInNow.className = "login__signin-now";
+
+let h1 = document.createElement('h1');
+h1.textContent = "Sign up NOW!";
+
+let h3 = document.createElement('h3');
+h3.textContent = "Enter your details"
+
+let form = document.createElement('form');
+
+let labelEmail = document.createElement('label');
+labelEmail.textContent = "Email";
+
+let inputEmail = document.createElement('input');
+inputEmail.className = "input-field";
+inputEmail.type = "email";
+inputEmail.placeholder = "Email";
+inputEmail.required = true
+
+let labelPassword = document.createElement('label');
+labelPassword.textContent = "Password";
+
+let inputPassword = document.createElement('input');
+inputPassword.className = "input-field";
+inputPassword.type = "password";
+inputPassword.placeholder = "Password";
+inputPassword.required = true
+
+let labelConfirmPassword = document.createElement('label');
+labelConfirmPassword.textContent = "Confirm Password";
+
+let inputConfirmPassword = document.createElement('input');
+inputConfirmPassword.className = "input-field";
+inputConfirmPassword.type = "password"; // cPassword? 
+inputConfirmPassword.placeholder = "Confirm Password";
+inputConfirmPassword.required = true
+
+let button = document.createElement('button');
+button.className = "filled-btn";
+button.id = "submit-btn";
+button.textContent = "Submit";
+
+labelConfirmPassword.appendChild(inputConfirmPassword);
+labelPassword.appendChild(inputPassword);
+labelEmail.appendChild(inputEmail);
+form.append(labelEmail, labelPassword, labelConfirmPassword, button);
+loginSignInNow.append(h1, h3, form);
+loginSignIn.appendChild(loginSignInNow);
+loginContainer.appendChild(loginSignIn);
+container.appendChild(loginContainer);
+document.body.appendChild(container);
+
+// Step 1.1. 
+
+let passwordError = document.createElement('p');
+labelConfirmPassword.appendChild(passwordError);
+
+button.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (inputPassword.value !== inputConfirmPassword.value) {
+        passwordError.textContent = "Passwords don't match.";
+    }
+    else {
+        window.location.href = "quotes.html";
+    }
+});
+
+// Step 2 
+
